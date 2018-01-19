18d19cda5fcaeec311768a983095a3a91c3d45bf
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-01-19 16:14:04 +0200

    Ajankäyttötestiä

--- src/main/java/main/Tiralabra.java
+++ src/main/java/main/Tiralabra.java
@@ -1,11 +1,13 @@
 package main;
 
 import java.io.IOException;
-import algorithm.FloydWarshall;
+import algorithm.PathSolver;
 import graph.Graph;
-import other.TestGraphs;
-import graph.implementations.AdjacencyMatrix;
+import graph.utils.PathResult;
 import graph.utils.UtilityMethods;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
 
 /**
  *
@@ -19,11 +21,47 @@ public class Tiralabra {
      */
     public static void main(String[] args) throws IOException {
 
-        /**
-         * Map from https://appro.limes.fi/restaurants.html
-         */
-        Application application = new Application("./helsinki.txt");
-        application.execute();
+//        try {
+//            int upto = Integer.parseInt(args[0]);
+            test(15);
+//        } catch (Exception e) {
+//            /**
+//             * Map from https://appro.limes.fi/restaurants.html
+//             */
+//            Application application = new Application("./helsinki.txt");
+//            application.execute();
+//        }
+
+    }
+
+    public static void test(int upto) throws IOException {
+        Graph graph = UtilityMethods.adjacencyMatrixFromListOfStrings(Files.readAllLines(Paths.get("./helsinki.txt")));
+        PathSolver solver = new PathSolver();
+
+        for (int i = 1; i < upto + 1; i++) {
+
+            long runtime = 0;
+
+            for (int j = 0; j < 5; j++) {
+                PathResult result = solver.solvePath(graph, generatePoints(i));
+                runtime += result.getRunningTimeMs();
+            }
+            System.out.println("With " + i + " points, running time: " + (runtime / 5) + " ms");
+        }
+    }
+
+    public static int[] generatePoints(int howMany) {
+        Random random = new Random();
+
+        int[] array = new int[howMany];
+
+        for (int i = 0; i < howMany; i++) {
+
+            array[i] = random.nextInt(41);
+
+        }
+
+        return array;
     }
 
 }
